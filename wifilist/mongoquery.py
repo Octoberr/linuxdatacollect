c8687dfa7d153b579b59169a0f16caffb398ecba
linxi <1353480158@qq.com>
2018-03-02 15:57:45 +0800

    new data

--- wifilist/mongoquery.py
+++ wifilist/mongoquery.py
@@ -17,9 +17,9 @@ def getquerydate():
     client = pymongo.MongoClient(host="192.168.1.213", port=27017)
     db = client.swmdb
     collection = db.wifilist
-    cursor = collection.find({"unixtime": {"$gt": int(today)}}, {"_id": 0}).sort({"unixtime": -1})
+    # cursor = collection.find({"unixtime": {"$gt": int(today)}}, {"_id": 0}).sort({"unixtime": -1})
     # cursor = collection.find({"unixtime": {"$lte": int(today)}}, {"_id": 0}).sort([("unixtime", -1)])
-    # cursor = collection.findall().sort([("unixtime", -1)])
+    cursor = collection.find({}, {"_id": 0}).sort([("unixtime", -1)])
     count = 0
     for el in cursor:
         havedate = datetime.datetime.fromtimestamp(int(el['unixtime'])).strftime('%Y-%m-%d %H:%M:%S')
@@ -30,6 +30,5 @@ def getquerydate():
             break
     jsondata = json.dumps(res)
     return jsondata
-
 
 
